583025de43769eb665396f9ac4675744627fc606
yupji <kxchi@naver.com>
2023-03-24 15:49:25 +0900

    feat: 에러메세지를 범용적으로 처리하게끔 메시지를 전달하여 처리하게끔 수정

--- basic/f5.py
+++ basic/f5.py
@@ -50,7 +50,9 @@ def login():
             pass
         else:       # 4. 회원아니면
             # 4-1. 적당한 메시지후 다시 로그인 유도
-            return render_template('error.html')
+            # render_template() => jinja2 템플릿 엔진 사용
+            # 문법 jinja2를 따라간다
+            return render_template('error.html', msg='로그인 실패')
 
 if __name__ == "__main__":
     app.run(debug=True)
